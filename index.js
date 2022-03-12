411a272bfa50fa01330c35871a4f53a4dff7c598
Lanzer <hdsnpej@gmail.com>
2022-03-12 10:20:30 -0700

    arrgle animacion

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const btnTijera = document.querySelector('.tijera');
 const winMessage = document.querySelector('.win-message')
 const userHand = document.querySelector('.user-img');
 const pcHand = document.querySelector('.pc-img')
+const wins = document.querySelector('.wins')
+const loses = document.querySelector('.loses')
+
+// shortcuts
+const u = userHand.classList;
+const p = pcHand.classList;
 
 // Logica de botones
 let userElection = '';
@@ -25,7 +31,6 @@ btnTijera.onclick = () => {
 
 // este codigo es para evitar que hagan muchas entradas xd
 
-
 const disable = () => {
     btnPiedra.disabled = 'true'
     btnPapel.disabled = 'true'
@@ -35,37 +40,53 @@ const disable = () => {
         btnPiedra.removeAttribute("disabled")
         btnPapel.removeAttribute("disabled")
         btnTijera.removeAttribute("disabled")
-        
-    }, 3000);
+        u.contains('shakeU') ? u.remove('shakeU') : null
+        p.contains('shakeP') ? p.remove('shakeP') : null
+        wins.textContent = winCounter;
+        loses.textContent = loseCounter;
+    }, 4000);
 }
 
-// logica del juego 
 
+// logica del juego 
+let winCounter = 0;
+let loseCounter = 0;
 let pcElection;
+
 const game = () => {
+
+    
     winMessage.textContent = ''
     disable()
+
+
     let pc_election = Math.floor(Math.random() * 3);
     if (pc_election === 0) {
         if (userElection === 'piedra') {
             wait('TIED!') // pc: piedra / user: piedra
         } else if (userElection === 'papel') {
+            winCounter++;
             wait('WIN!') // pc: piedra / user: papel
         } else {
+            loseCounter++;
             wait('LOSE!') // pc: piedra / user: tijera
         }
     } else if (pc_election === 1) {
         if (userElection === 'piedra') {
+            loseCounter++;
             wait('LOSE!') //pc: papel / user: piedra
         } else if (userElection === 'papel') {
             wait('TIED!') //pc: papel / user: papel
         } else {
+            winCounter++;
             wait('WIN!') //pc: papel / user: tijera
         }
     } else if (pc_election === 2) {
         if (userElection === 'piedra') {
+            winCounter++;
             wait('WIN!') //pc: tijera / user: piedra
         } else if (userElection === 'papel') {
+            loseCounter++;
             wait('LOSE!') //pc: tijera / user: papel
         } else {
             wait('TIED!') //pc: tijera / user: tijera
@@ -77,8 +98,6 @@ const game = () => {
 // animacion y efectos
 
 const animation = (userEl) => {
-    const u = userHand.classList;
-    const p = pcHand.classList;
 
     userHand.src = './assets/rock.svg'
     pcHand.src = './assets/rock.svg'
